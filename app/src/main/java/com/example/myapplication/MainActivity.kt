969043ab2f3fdc668eff98b2eee7f0504ae9777d
croc <j03114330@hotmail.com>
2024-03-26 20:53:09 +1100

    login page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,22 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,50 +61,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LoginPag() {
     var phoneNumber by remember { mutableStateOf("") }
-    Column (modifier = Modifier.padding(all = 0.dp)){
-        Image(painter = painterResource(id = R.drawable.eac73dd3dafdc6bb0cc84cf18213072), contentDescription = null)
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = "Welcome to",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 20.dp),
-            fontSize = 22.sp,
-            style = MaterialTheme.typography.bodyLarge
-            )
+    Column(modifier = Modifier.padding(all = 0.dp)) {
+        Image(painter = painterResource(id = R.drawable.firstpagepeople), contentDescription = null)
+        Spacer(modifier = Modifier.height(10.dp))
         Image(
-            painter = painterResource(id = R.drawable.monashpark), contentDescription = null,
+            painter = painterResource(id = R.drawable.cellopark), contentDescription = null,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .size(100.dp),
         )
 
-        Spacer(modifier = Modifier.height(100.dp))
-        Row (modifier = Modifier.absolutePadding(left = 20.dp)){
-            if (phoneNumber.isNotEmpty()){
-                Text(
-                    text = "      Enter you mobile number",
-                    fontSize = 20.sp,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
-            Icon(
-                imageVector = Icons.Default.Phone,
-                contentDescription = "phone",
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(modifier = Modifier.absolutePadding(left = 10.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.phone), contentDescription = null,
                 modifier = Modifier
-                    .size(24.dp)
-                    .offset(y = 2.dp)
+                    .size(50.dp)
+                    .offset(20.dp)
             )
-            Text(
-                text = "      Enter you mobile number",
-                fontSize = 20.sp,
-                style = MaterialTheme.typography.bodyLarge
+            Spacer(modifier = Modifier.width(15.dp))
+            OutlinedTextField(
+                value = phoneNumber,
+                onValueChange = { phoneNumber = it },
+                label = { Text(text = "Enter you mobile number") },
+                modifier = Modifier
+                    .padding(0.dp)
+                    .border(width = 3.dp, color = Color.White)
             )
         }
-        Divider(color = Color.LightGray, thickness = 1.dp)
+
+
     }
-
-
 }
 
 
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.icons.Icons
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.Color
-
-
